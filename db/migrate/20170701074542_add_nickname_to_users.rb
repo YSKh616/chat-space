7a6902f9955677b9df9f46b25352c6c5c38dae51
日比野佑亮 <hibino616@gmail.com>
2017-07-01 18:26:02 +0900

    Modified files
    ①Gemfileのerb2hamlの場所修正
    ②nameカラムにindex, null: false, unique: trueを追加。

--- db/migrate/20170701074542_add_nickname_to_users.rb
+++ db/migrate/20170701074542_add_nickname_to_users.rb
@@ -1,5 +1,5 @@
 class AddNicknameToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :name, :string
+    add_column :users, :name, :string, unique:true, null: false
   end
 end
