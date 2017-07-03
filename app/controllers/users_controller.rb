eab26ade3ed30b78b345da944bd4778f78235891
日比野佑亮 <hibino616@gmail.com>
2017-07-03 12:09:19 +0900

    create update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,12 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
+  private
+  def user_params
+    params.permit(:name, :email)
   end
 end
