bea6e9590c4e1241a1f3e4ee38d5d61aff1025b7
日比野佑亮 <hibino616@gmail.com>
2017-07-04 15:47:22 +0900

    Make chat-edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,9 +13,16 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: "グループが更新されました"
+    else
+      render :new
+    end
   end
 
   private
