c4dc9cc5612d31b4a9708918d0a1c521fea6af13
日比野佑亮 <hibino616@gmail.com>
2017-07-05 20:24:00 +0900

    Create message send

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,17 +4,29 @@ class MessagesController < ApplicationController
   def index
   end
 
-  def new
-  end
-
   def create
+    @message = current_user.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@message.group_id), notice: "メッセージが送信されました"
+    else
+      @group = Group.find(current_user.id)
+      @groups = current_user.groups
+      flash[:alert] = 'メッセージを入力してください'
+      render :index
+    end
   end
 
 
   private
 
   def set_group
+    @group = Group.find(current_user.id)
     @groups = current_user.groups
+    @message = Message.new
+  end
+
+  def message_params
+    params.require(:message).permit(:body, :image, :group_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'groups#index'
+  root  'messages#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:edit, :update, :new, :create, :index] do
     resources :messages, only: [:index, :new, :create]
