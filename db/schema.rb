2068c96409508df315598ff017740b803ed1b874
日比野佑亮 <hibino616@gmail.com>
2017-07-06 13:50:43 +0900

    Modified migration file

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20170705054215) do
     t.integer  "group_id"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -57,4 +59,6 @@ ActiveRecord::Schema.define(version: 20170705054215) do
 
   add_foreign_key "groups_users", "groups"
   add_foreign_key "groups_users", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
