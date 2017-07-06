03f61bfd5c537a5b79520ad63ddf7a64c76e5ce4
日比野佑亮 <hibino616@gmail.com>
2017-07-06 17:10:23 +0900

    Add method
    グループの最新メッセージを表示する部分をメソッド化

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :groups_users
   has_many :groups_users
   has_many :messages
+
+  def find_last_message
+    unless messages.empty?
+      messages.last.body
+    else
+      "まだメッセージはありません。"
+    end
+  end
 end
