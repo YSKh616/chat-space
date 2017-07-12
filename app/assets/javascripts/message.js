0b560c2e638e8629aebdd598687fd86a6afc77ee
日比野佑亮 <hibino616@gmail.com>
2017-07-12 09:25:39 +0900

    make scroll & changed Asynchronous communication
    ①html内のif文の記述を変更。
    ②②回目以降の送信を可能にした。
    ③最新メッセージへスクロールするようにした。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,26 @@
 $(function() {
   function buildHTML(message){
-    var html = `<p class="chat-display__main--sender">
-                  ${message.user_name}
-                <span>
-                  ${message.created_at}
-                <p class="chat-display__main--chat">
-                  ${message.body}
-                </p>`
-                <% if(message.image) %>
-                  `<p class="chat-display__main--chat">
-                      <image src="${message.image}">
-                  </p>`
-                <% end %>
+    var html = `<div class="chat-display__main__message">
+                  <p class="chat-display__main__message--sender">${message.user_name}</p>
+                  <span>${message.created_at}</span>
+                  <p class="chat-display__main__message--chat">${message.body}</p>`;
+    if(message.image.url) {
+      html += `<p class="chat-display__main__message--chat-image">
+                 <image src="${message.image.url}">
+              </p></div>`;
+    } else {
+      html += `<p class="chat-display__main__message--chat-image"></p></div>`;
+    }
     return html;
   }
   $('#send_message').on('submit', function(e) {
     e.preventDefault();
-    var url = window.location.href;
+    var url = $(this).prop("action");
     var form = $('#send_message').get()[0];
     var formData = new FormData(form);
-    console.log(formData);
+    // $('.chat-display__main').animate({scrollTop: $('.chat-display__main').height()}, 'fast');
+    // $('.chat-display__main').animate({scrollTop: $('.chat-display__main').scrollHeight}, 'fast');
+    // console.log(formData);
     $.ajax({
       type: 'POST',
       url: url,
@@ -30,8 +31,11 @@ $(function() {
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.chat-display__main').append(html)
-      $('.text').val('')
+      var position = $('.chat-display__main').find($(".chat-display__main__message:last-child")).get(0).offsetTop
+      $('.chat-display__main').append(html).not(":animated").animate({scrollTop:position});
+      $('.text').val('');
+      $('.image-upload').val('');
+      $(".message__button").prop("disabled",false);
     })
     .fail(function(){
       alert('error');
